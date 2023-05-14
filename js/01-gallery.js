d1dc6907634f8df42f2b9d8210120a17b82a96a5
NesterovSerhii <sergei_nesterov8@ukr.net>
2023-05-14 19:08:18 +0300

    added libraries, completed tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,67 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const list = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryElements = [];
+
+galleryItems.forEach(galleryItem => {
+    
+    const item = document.createElement('li');
+    item.classList.add('gallery__item');
+    
+    
+    const link = document.createElement('a');
+    link.classList.add('gallery__link');
+    link.href = galleryItem.original;
+    
+    const img = document.createElement('img');
+    img.classList.add('gallery__image');
+    img.dataset.source = galleryItem.original;
+    img.src = galleryItem.preview;
+    img.alt = galleryItem.description;
+    
+    item.appendChild(link);
+    link.appendChild(img);
+
+    galleryElements.push(item); 
+});
+
+list.append(...galleryElements);
+
+
+list.addEventListener('click', onGalleryImageClick);
+
+function onGalleryImageClick(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    const imageSrc = event.target.dataset.source;
+    const instance = basicLightbox.create(`<img src="${imageSrc}" >`,
+  {
+    onShow: () => {
+      window.addEventListener('keydown', onPressEscKey);
+    },
+    onClose: () => {
+      window.removeEventListener('keydown', onPressEscKey);
+    },
+  });
+    
+    instance.show();
+
+    window.addEventListener("keydown", onCloseModal);
+
+    function onCloseModal(event) {
+    if (event.code === "Escape") {
+        instance.close();
+      }
+    }
+    
+    function onPressEscKey(event) {
+      if (event.code !== 'Escape') {
+        return;
+      }
+        instance.close();
+    }
+}
